e826e4126feb3552b9d3d4cac66cfaba46f8f793
Ninhninh2010 <vanninh0822010@gmail.com>
2023-12-24 10:22:20 +0700

    ninh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/LoginADmin/LoginAdmin";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="admin" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="admin" />} />
+        <Route path="login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
